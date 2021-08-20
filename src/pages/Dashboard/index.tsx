0c7b8932112e42d913b37f4f07513d7797dca35a
thuiskon <tuco.kerber@gmail.com>
2021-08-20 13:48:09 -0300

    c03 - consumo da api e montagem da lista

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,18 +1,63 @@
 import React from 'react';
-import { Title, Form } from './styles'
+import { Title, Form, Repos } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import { api } from '../../services/api';
 
-import  logo  from '../../assets/logo.svg';
+import logo from '../../assets/logo.svg';
+import { useState } from 'react';
 
+
+interface GithubRepository {
+    full_name: string,
+    description: string,
+    owner:{
+        login: string,
+        avatar_url: string
+    }
+}
 
 export const Dashboard: React.FC = () => {
+    const [repos, setRepos] = useState<GithubRepository[]>([]);
+    const [newRepo, setNewRepo] = useState<string>('');
+
+    function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) : void {
+        setNewRepo(event.target.value);
+    }
+
+    async function handleAddRepo(event: React.FormEvent<HTMLFormElement>): Promise<void> {
+        event.preventDefault();
+
+        const response = await api.get<GithubRepository>(`/repos/${newRepo}`);
+        
+        const repository = response.data;
+        setRepos([...repos, repository]);
+        setNewRepo('');
+    }
+
     return (
         <>
             <img src={logo} alt="GitCollection" />
             <Title>Catalogo de repositórios do Github</Title>
-            <Form>
-                <input placeholder="username/repository_name" />
+            <Form onSubmit={ handleAddRepo }>
+                <input placeholder="username/repository_name"
+                    value={newRepo}
+                    onChange={ handleInputChange } />
                 <button type="submit" >Buscar</button>
             </Form>
+
+            <Repos>
+                {repos.map(item =>(
+                    <a href="/repositories" key={item.full_name}>
+                    <img src={item.owner.avatar_url}
+                     alt={item.owner.login} />
+                    <div>
+                        <strong>{item.full_name}</strong>
+                        <p>{item.description}</p>
+                    </div>
+                    <FiChevronRight size={20}/>
+                </a>
+                ))}
+            </Repos>
         </>
 
     );
