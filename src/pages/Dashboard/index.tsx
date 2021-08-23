29822f31e668a67954c86034559ed01199a06065
thuiskon <tuco.kerber@gmail.com>
2021-08-23 14:35:46 -0300

    c05 - correcao da key dos links

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -8,87 +8,101 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 
 import { Link } from 'react-router-dom';
+import { useRef } from 'react';
 
 
 interface GithubRepository {
     full_name: string,
     description: string,
-    owner:{
+    owner: {
         login: string,
         avatar_url: string
     }
 }
 
 export const Dashboard: React.FC = () => {
-    const [repos, setRepos] = useState<GithubRepository[]>(()=>{
+    const [repos, setRepos] = useState<GithubRepository[]>(() => {
         const storageRepos = localStorage.getItem('@GitCollection:repositories');
-        if(storageRepos){
-           return JSON.parse(storageRepos);
+        if (storageRepos) {
+            return JSON.parse(storageRepos);
         } else {
             return [];
         }
     });
     const [newRepo, setNewRepo] = useState('');
     const [inputError, setInputError] = useState('');
+    const formEl = useRef<HTMLFormElement | null>(null);
 
-    
-    useEffect(()=>{
+
+    useEffect(() => {
         localStorage.setItem('@GitCollection:repositories', JSON.stringify(repos))
     }, [repos]);
 
-    
 
-    function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) : void {
+
+    function handleInputChange(event: React.ChangeEvent<HTMLInputElement>): void {
         setNewRepo(event.target.value);
     }
 
     async function handleAddRepo(event: React.FormEvent<HTMLFormElement>): Promise<void> {
         event.preventDefault();
 
-        if(!newRepo){
+        if (!newRepo) {
             setInputError('Informe o username/repositorio');
             return;
         }
-        const response = await api.get<GithubRepository>(`/repos/${newRepo}`);
-        
-        const repository = response.data;
-        setRepos([...repos, repository]);
-        setNewRepo('');
+        try {
+            const response = await api.get<GithubRepository>(`/repos/${newRepo}`);
+
+            const repository = response.data;
+            setRepos([...repos, repository]);
+            formEl.current?.reset();
+            setNewRepo('');
+            setInputError('');
+
+        } catch  {
+            setInputError('Repositorio nao encontrado no GitHub');
+        }
     }
 
-    function handleClear(){
+    function handleClear() {
         localStorage.removeItem('@GitCollection:repositories');
         setRepos([]);
     }
+
+    
 
     return (
         <>
             <img src={logo} alt="GitCollection" />
             <Title>Catalogo de repositórios do Github</Title>
-            <Form hasError={Boolean(inputError)} onSubmit={ handleAddRepo }>
+            <Form
+                ref={formEl}
+                hasError={Boolean(inputError)}
+                onSubmit={handleAddRepo}>
+
                 <input placeholder="username/repository_name"
-                    value={newRepo}
-                    onChange={ handleInputChange } />
+                    onChange={handleInputChange} />
                 <button type="submit" >Buscar</button>
             </Form>
-            {inputError && <Error>{ inputError }</Error>}
-            
+            {inputError && <Error>{inputError}</Error>}
+
             <Repos>
-                {repos.map(item =>(
-                    <Link to={`/repositories/${item.full_name}`} key={item.full_name}>
-                    <img src={item.owner.avatar_url}
-                     alt={item.owner.login} />
-                    <div>
-                        <strong>{item.full_name}</strong>
-                        <p>{item.description}</p>
-                    </div>
-                    <FiChevronRight size={20}/>
-                </Link>
+                {repos.map((item, index) => (
+                    <Link to={`/repositories/${item.full_name}`} key={item.full_name + index}>
+                        <img src={item.owner.avatar_url}
+                            alt={item.owner.login} />
+                        <div>
+                            <strong>{item.full_name}</strong>
+                            <p>{item.description}</p>
+                        </div>
+                        <FiChevronRight size={20} />
+                    </Link>
                 ))}
             </Repos>
             <>
-                { repos && <BtnClear onClick={handleClear}>Limpar Cache</BtnClear>}
-                
+                {repos && <BtnClear onClick={handleClear}>Limpar Cache</BtnClear>}
+
             </>
         </>
 
